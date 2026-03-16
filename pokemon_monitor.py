7947fc3c2315b5233fccebca223ae58d5c374516
swaltzy <98061318+swaltzy@users.noreply.github.com>
2026-03-16 18:17:43 +0000

    Update pokemon_monitor.py

--- pokemon_monitor.py
+++ pokemon_monitor.py
@@ -19,8 +19,10 @@ def check():
         "q": "trainer box",
         "format": "json"
     }
-
-    r = requests.get(API, params=params)
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36"
+}
+    r = requests.get(API, params=params, headers=headers)
     data = r.json()
 
     for product in data.get("results", []):
